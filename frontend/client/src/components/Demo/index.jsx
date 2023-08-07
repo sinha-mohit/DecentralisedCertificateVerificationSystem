8db236ab34c1742950be14ed5d678d3520109054
Mohit Sinha <mohitsinha1994@gmail.com>
2023-08-07 14:09:11 +0530

    Develop (#5)
    * Update check_functionality script
    
    * Update check_functionality script
    
    * Add UI changes
    
    * Update UI
    
    * Update artifact check
    
    * Update UI
    
    * Update artifact check

--- frontend/client/src/components/Demo/index.jsx
+++ frontend/client/src/components/Demo/index.jsx
@@ -1,33 +1,25 @@
-import { useState } from "react";
 import useEth from "../../contexts/EthContext/useEth";
 import Title from "./Title";
-import Cta from "./Cta";
-import Contract from "./Contract";
 import ContractBtns from "./ContractBtns";
-// import Desc from "./Desc";
 import NoticeNoArtifact from "./NoticeNoArtifact";
 import NoticeWrongNetwork from "./NoticeWrongNetwork";
 
 function Demo() {
   const { state } = useEth();
-  const [value, setValue] = useState("?");
 
   const demo =
     <>
-      <Cta />
       <div className="contract-container">
-        <Contract value={value} />
-        <ContractBtns setValue={setValue} />
+        <ContractBtns />
       </div>
-      {/* <Desc /> */}
     </>;
 
   return (
     <div className="demo">
       <Title />
       {
-        !state.artifact_simpleStorage ? <NoticeNoArtifact /> :
-          !state.contract_simpleStorage ? <NoticeWrongNetwork /> :
+        !state.artifact_regulatedEntity ? <NoticeNoArtifact /> :
+          !state.artifact_regulatedEntity ? <NoticeWrongNetwork /> :
             demo
       }
     </div>
